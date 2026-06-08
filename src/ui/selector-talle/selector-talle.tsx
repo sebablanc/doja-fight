2449aae8ab12b120ae42cde15ba6e695ce9d486f
sebablanc <blancseba@gmail.com>
2026-06-08 09:53:06 -0300

    se quita codigo comentado

--- src/ui/selector-talle/selector-talle.tsx
+++ src/ui/selector-talle/selector-talle.tsx
@@ -1,38 +1,15 @@
 import { useState } from 'react';
 import './selector-talle.css'
 import TablaTalleModal from './tabla-talle-modal';
-// import { TalleDispatchContext } from '../../core/talle/talle-context';
-
-// const TALLES = [
-//     { value: "S", label: "S" },
-//     { value: "M", label: "M" },
-//     { value: "L", label: "L" },
-//     { value: "XL", label: "XL" },
-//     { value: "2XL", label: "2XL" }
-// ]
 
 function SelectorTalle() {
     const [openModal, setOpenModal] = useState(false);
-    // const dispatch = useContext(TalleDispatchContext);
-
     const closeModal = () => setOpenModal(false);
-
-    // const handleSelectChange = (e: any) => {
-    //     if (dispatch) {
-    //         dispatch(e.target.value);
-    //     }
-    // }
 
     return (
         <>
             <TablaTalleModal isOpen={openModal} onClose={closeModal} />
             <a className='tabla-talle-button common-button' onClick={() => setOpenModal(true)}>Tabla de talles</a>
-            {/* <section className="selector-talle-container">
-                <p>Selecciona tu talle:</p>
-                <select id='selector_talles' onChange={handleSelectChange}>
-                    {TALLES.map(talle => <option key={`talle_${talle.value}`} value={talle.value}>{talle.label}</option>)}
-                </select>
-            </section> */}
         </>
     )
 }
